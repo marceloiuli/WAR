ea85e4bf7c865b35b5cd08624623e1a326ccadb0
marceloiuli <marceloiuli26@gmail.com>
2023-08-07 10:31:21 -0700

    scaled card size up, added reset btn  & added event listeners

--- js/main.js
+++ js/main.js
@@ -10,16 +10,26 @@ let winner; //update message & show reset btn
 const messageEl = document.querySelector('h2')
 const playDeck = document.getElementById('playDeck')
 const playWar = document.getElementById('playWar')
-
+const comWar = document.getElementById('comWar')
+const resetBtn = document.querySelector('button')
 /*----- event listeners -----*/
 document.addEventListener('click', info)
 function info(evt){
     console.log(evt.target)
 }
 
+playDeck.addEventListener('click', handleCard)
+playWar.addEventListener('click', handleScore)
+resetBtn.addEventListener('click', init)
 /*----- functions -----*/
 init();
-//initialize all state, then call render
+//initialize all state, then call render()
 function init() {
+
+    render()
+}
+
+//stub up render func to update state
+function render() {
     
 }
