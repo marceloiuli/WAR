d8cfe6897f9ca7847c4a25d6a970325019c3a318
marceloiuli <marceloiuli26@gmail.com>
2023-08-06 19:33:41 -0700

    removed winPile, updated css & html, added js starter code, changed board design

--- js/main.js
+++ js/main.js
@@ -2,12 +2,24 @@
 
 
 /*----- state variables -----*/
-
+let board; //update card(s) played on board
+let turn; //update message & score
+let winner; //update message & show reset btn
 
 /*----- cached elements  -----*/
-
+const messageEl = document.querySelector('h2')
+const playDeck = document.getElementById('playDeck')
+const playWar = document.getElementById('playWar')
 
 /*----- event listeners -----*/
-
+document.addEventListener('click', info)
+function info(evt){
+    console.log(evt.target)
+}
 
 /*----- functions -----*/
+init();
+//initialize all state, then call render
+function init() {
+    
+}
