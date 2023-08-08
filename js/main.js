f213a94a7091c6ed347b9ebcfa3e5d31ff55b459
marceloiuli <marceloiuli26@gmail.com>
2023-08-08 14:29:44 -0700

    created 2 shuffled decks for player and computer

--- js/main.js
+++ js/main.js
@@ -4,50 +4,56 @@ const DECK = [
 'c02', 'c03', 'c04', 'c05', 'c06', 'c07', 'c08', 'c09', 'c10', 'cJ', 'cQ', 'cK', 'cA',
 'd02', 'd03', 'd04', 'd05', 'd06', 'd07', 'd08', 'd09', 'd10', 'dJ', 'dQ', 'dK', 'dA',
 'h02', 'h03', 'h04', 'h05', 'h06', 'h07', 'h08', 'h09', 'h10', 'hJ', 'hQ', 'hK', 'hA'
-]
-
+];
 
 /*----- state variables -----*/
-let board; //update card(s) played on board
-let turn; //update message & score
-let winner; //update message & show reset btn
+let playerDeck = [];
+let computerDeck = [];
 
 /*----- cached elements  -----*/
-const messageEl = document.querySelector('h2')
+const boardEl = document.getElementsByClassName('board')
+const msgEl = document.querySelector('h2')
 const playDeck = document.getElementById('playDeck')
 const playWar = document.getElementById('playWar')
 const comDeck = document.getElementById('comDeck')
 const comWar = document.getElementById('comWar')
 const resetBtn = document.querySelector('button')
 /*----- event listeners -----*/
-document.addEventListener('click', info)
-function info(evt){
-    console.log(evt.target)
-}
+// document.addEventListener('click', info)
+// function info(evt){
+//     console.log(evt.target)
+// }
 
-playDeck.addEventListener('click', handleCards)
-playWar.addEventListener('click', handleScore)
-resetBtn.addEventListener('click', init)
+// playDeck.addEventListener('click', handleCards)
+// playWar.addEventListener('click', handleScore)
+// resetBtn.addEventListener('click', initialize)
 /*----- functions -----*/
-init();
+initialize();
 //initialize all state, then call render()
-function init() {
-    
-    render()
+function initialize() {
+    const shuffledDeck = shuffle(DECK)
+    playerDeck = shuffledDeck.slice(0, 26)
+    computerDeck = shuffledDeck.slice(26)
+    //render()
 }
 
 //stub up render func to update state
 function render() {
-
+    renderBoard()
+    renderTurn()
+    renderScore()
+    renderWinner()
 }
 
 function shuffle(arr) {
     const shuffledDeck = [...DECK];
-    const length = shuffledDeck.length;
-    for(let i = 0; i < length; i++) {
-        const randomOrder = Math.floor((shuffledDeck.length - i) * Math.random())
-        const randomDeck = shuffledDeck.splice(randomOrder, 1)
-        shuffledDeck.push(...randomDeck)
+    for(let i = 0; i < shuffledDeck.length; i++) {
+        const randomOrder = Math.floor((shuffledDeck.length - i) * Math.random());
+        const randomDeck = shuffledDeck.splice(randomOrder, 1);
+        shuffledDeck.push(...randomDeck);
     }
-    return shuffledDeck
+    return shuffledDeck;
 }
+
+// playerDeck = shuffledDeck.slice(0, 26)
+// computerDeck = shuffledDeck.slice(26)
--- readme.md
+++ readme.md
@@ -1,27 +1,23 @@
 #WAR
 <---:WAR PSUEDO CODE:--->
 
-- User will open application with card table and full single deck of cards in the middle of the table ()
+- User will open application with card tables and shuffled single deck of cards on player and computer table (26 cards each)
 
-- When player presses "play button" the cards will be shuffled then dealt between them and computer (1/2 deck)
-
-- When player clicks on their deck, their card will be placed "face up" in front of their deck, while simultaneously the computer's card will be face up in front of their deck
+- When player clicks on their deck, their card will be placed "face up" next to their deck, while simultaneously the computer's card will be face up next to their deck
 
 - Compare the higher card between both
 
-- After comparing for the higher card, the winner will get both cards shifted to their face up winning pile
+- After comparing for the higher card, the message beteen the tables will update for win or loss or tie
 
-    - if both cards are equal (THIS MEANS WAR) the player and computer will draw their next top 2 cards and play the third card (loop)
+    - if both cards are equal(tie) THIS MEANS WAR message will update the player and computer will draw their next card and play
 
     - if they tie again repeat process until winner
 
-- Whomever wins will get those cards added to their win pile
+- Whomever wins will get those cards added to their score
 
-- Add points for each card in winning pile
+- compare scores when all cards are gone and highest score wins
 
-- compare scores when all cards are gone and highest score wins a point for each card (+2) after a war (+6)?
-
-- if the scores are a tie display a message saying (the war is a draw).
+- if the scores at end of game are a tie display a message saying (the war is a draw).
 
 - Repeat click and flip until either computers cards run out or the players cards run out
 
@@ -29,4 +25,4 @@
 
 - After the winner is displayed, display a button to reset game board
 
-- When reset button is clicked, reinitialize board to starting board
+- When reset button is clicked, reinitialize current board to starting board 
